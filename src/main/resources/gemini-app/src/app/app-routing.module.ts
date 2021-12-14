bebf6c1144d2629064673ba23e03415333f03d8f
Marcelo Victor Sousa Rosa <vyctor.rosa14@gmail.com>
2021-12-14 20:48:23 -0300

    Crud v2

--- src/main/resources/gemini-app/src/app/app-routing.module.ts
+++ src/main/resources/gemini-app/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { QuestionarioComponent } from './pages/questionario/questionario.component';
 import { InitialComponent } from './pages/initial/initial.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',component:InitialComponent}
+  {path:'',component:InitialComponent},
+  {path:'questionario',component:QuestionarioComponent},
+
 ];
 
 @NgModule({
